47abf6f73d8782a6ba923ad69c8a346dbb6014c9
hayden@haydos <haydenmorton26@gmail.com>
2022-05-05 19:16:32 +0800

    AI Struct as unit.h

---
+++ unit.h
@@ -0,0 +1,13 @@
+#ifndef UNIT_H
+	#define UNIT_H
+
+	typedef struct {
+		int position;
+		int health;
+		int damage;
+		int colour[4];
+		bool direction;
+		bool enemy;
+		int cost;
+	} Unit
+#endif
